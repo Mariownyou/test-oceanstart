98b65065bc38dd9578fc00808c29155916f2b778
Leva <smj510black@gmail.com>
2022-01-07 22:13:48 +0300

    added models basic structure

--- app/categories/models.py
+++ app/categories/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField()
+
+    def __str__(self):
+        return f'Category: {self.name}'
+
--- app/products/models.py
+++ app/products/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField()
+    categories = models.ManyToManyField(
+        'Category',
+        on_delete=models.PROTECT,
+        related_name='products'
+    )
+    category = models.CharField()
+    price_min = models.PositiveIntegerField()
+    price_max = models.PositiveIntegerField()
+    is_published = models.BooleanField(default=False)
+    is_removed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'Product: {self.name}'
+
